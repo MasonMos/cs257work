34148fccf73a4d4f976501b207d381928a247125
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-04-12 10:33:02 -0500

    add new inclasssqllab

---
+++ inclasssqllab/createtableCities.sql
@@ -0,0 +1,8 @@
+DROP TABLES IF EXISTS cities;
+ CREATE TABLE cities (
+  City text,
+  State text,
+  Population int,
+  Latitude real,
+  Longitude real
+);
---
+++ inclasssqllab/createtableStates.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS states;
+ CREATE TABLE states (
+    Code text,
+    State text,
+    Population int
+ )
