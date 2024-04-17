4264b59eebfd75af2e30a2aa824b2fb17b21bd65
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-04-17 13:53:58 -0500

    made largest city pop query

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -25,7 +25,35 @@ def test_query_northfield():
 
     return row
 
+def test_query_largest_city():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="mosesm2",
+        user="mosesm2",
+        password="field599farm")
+    
+    cur = conn.cursor()
+
+    sql = "SELECT city, city_population FROM cities"
+
+    cur.execute(sql)
+
+    row_list = cur.fetchall()
+
+    for row in row_list:
+        print(row[1])
+
+    conn.commit()
+
+    return None
+
+
+
+
 print( test_query_northfield() )
+print( test_query_largest_city() )
     
 
 
