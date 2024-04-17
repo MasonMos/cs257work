8bd59f1bd3867b603aa66abcbe51b5f1c43e4301
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-04-17 12:18:33 -0500

    made cityquery file

---
+++ pythonlab/cityquery.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+# This function checks for northfield data.
+def test_query_northfield():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="mosesm2",
+        user="mosesm2",
+        password="field599farm")
+
+    cur = conn.cursor()
+
+    sql = "SELECT city, latitude, longitude FROM cities WHERE city = 'Northfield' "
+
+    cur.execute(sql)
+
+    row = cur.fetchone()
+
+    if row is None:
+        print("Northfield is not present in this data.")
+
+    conn.commit()
+
+    return row
+
+print( test_query_northfield() )
+    
+
+
+    
+
+
