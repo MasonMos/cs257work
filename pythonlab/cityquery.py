04939d50e8596cdbb9570ab3ef8ebdbc9eaffb39
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-04-20 13:36:25 -0500

    adding code

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -52,6 +52,36 @@ def test_query_largest_city():
     print(largestCityPop)
     return None
 
+def test_query_smallest_city_mn():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="mosesm2",
+        user="mosesm2",
+        password="field599farm")
+    
+    cur = conn.cursor()
+
+    sql1 = """SELECT * FROM cities
+            JOIN states;"""
+    
+    sql2 = """CREATE VIEW cities_states AS SELECT city, city_population, code, state
+            WHERE code = 'MN' """
+
+    cur.execute(sql1, sql2)
+
+    row_list = cur.fetchall()
+
+    cityPop = row[1]
+
+    for row in row_list:
+        if (cityPop <= row[1]):
+            cityPop = row[1]
+            smallestCityPop = row[0]
+
+    print(smallestCityPop)
+    return None
 
 
 
