49c8e0a949a750a98804ab6167b5c25d49787dd9
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-04-23 22:21:58 -0500

    added data table

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -1,4 +1,5 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
@@ -23,6 +24,33 @@ def my_sum(num1, num2):
     sum = num1int + num2int
     return str(sum)
 
+@app.route('/pop/<state>')
+def state_population(state):
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="mosesm2",
+        user="mosesm2",
+        password="field599farm")
+    
+    cur = conn.cursor()
+
+    sql = """SELECT city, cities.state, city_population, code 
+    from cities join states 
+    on states.state = cities.state WHERE code = %s OR cities.state = %s;"""
+
+    stateInput = input("Pick a state or state abbreviation:")
+
+    cur.execute(sql, (stateInput, stateInput))
+
+    row_list = cur.fetchall()
+
+    totalCityPop = 0
+    stateName = ""
+
+    for row in row_list:
+        totalCityPop += row[2]
+        stateName = row[1]
 
 if __name__ == '__main__':
     my_port = 5120
