8fa2bf214361c40378803f193bcebd5ca4f665b2
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-04-23 21:13:09 -0500

    changed code

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -18,8 +18,10 @@ def my_color(word1):
 
 @app.route('/add/<num1>/<num2>')
 def my_sum(num1, num2):
-    sum = int(num1) + int(num2)
-    return sum
+    num1int = int(num1)
+    num2int = int(num2)
+    sum = num1int + num2int
+    return str(sum)
 
 
 if __name__ == '__main__':
